7bfe7a11e36b28f4bd16b75f948534688d46622a
Amr-3009 <amrosama1k@gmail.com>
2023-05-13 07:21:47 -0700

    gg

--- test/execmd.c
+++ test/execmd.c
@@ -1,19 +1,36 @@
 #include "main.h"
 
-void execmd(char **av)
+int execmd(char **av)
 {
-char *command = NULL, *a_command = NULL;
+    char *command = NULL, *a_command = NULL;
+    pid_t pid;
+    int status;
 if (av)
 {
-/* command is at av index 0 (first token)*/
-command = av[0];
-/* generate path to the command*/
-a_command = get_location(command);
-/* command execution with execve*/
-if (execve(a_command, av, NULL) == -1)
-{
-perror("ERROR");
+    command = av[0];
+    a_command = get_location(command);
+    pid = fork();
+    if (pid == 0)
+    {
+        if (execve(a_command, av, NULL) == -1)
+        {
+            perror("ERROR cp");
+        }
+        exit(EXIT_FAILURE);
+    }
+    else if (pid < 0)
+    {
+            perror("ERROR f");
+    }
+    else
+    {
+        do
+        {
+            waitpid(pid, &status, WUNTRACED);
+        }
+        while(!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
 }
-}
+    return(-1);
 }
 
--- test/main.h
+++ test/main.h
@@ -4,9 +4,9 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 
-
-void execmd(char **av);
+int execmd(char **av);
 char *get_location(char *command);
 int stat(const char *pathname, struct stat *statbuf);
 
